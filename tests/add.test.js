0e2b7923a1e25e0ac21ba858e0c830389a1bffbe
Alexandre Leroux <alex@sherpa.one>
2022-11-24 22:54:04 +0100

    eslint

--- tests/add.test.js
+++ tests/add.test.js
@@ -1,4 +1,5 @@
 const { add } = require('../app');
+const { test, expect } = require('jest');
 
 test('adds 1 + 2 to equal 3', () => {
     expect(add(1, 2)).toBe(3);
--- tests/multiply.test.js
+++ tests/multiply.test.js
@@ -1,4 +1,5 @@
 const { multiply } = require('../app');
+const { test, expect } = require('jest');
 
 test('multiply 2 to 5 to equal 10', () => {
     expect(multiply(2, 5)).toBe(10);
